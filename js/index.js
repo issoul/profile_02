ba76744181bd596d15780a2728e7680284a95c87
issoul <153714324+issoul@users.noreply.github.com>
2024-01-29 11:37:31 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,40 +3,38 @@
 const navToggle = document.querySelector('.hamburger');
 const navMenu = document.querySelector('header');
 const menulist = document.querySelector('.menu_item');
-var ham = document.querySelector('#menu_icon');
-var clo = document.querySelector('#close_icon');
+
+const ham = document.querySelector('#menu_icon');
 
 
-    var state=["none","block"];
-    var i=0;
-    var j=1;
 
-    function change(){
-        if(i>=state.length){
-            i=0;
-        }
-        ham.style.display=state[i];
-        i++;
-
-        if(j<0){
-            j=1;
-        }
-        clo.style.display=state[j];
-        j--
-
-    }
-
-
+var i = 0;
 
 // 햄버거 버튼 클릭 이벤트 //
 navToggle.addEventListener('click', ()=> {
     navMenu.classList.toggle('open');
-    change();
+    // ham.classList.toggle('close');
+    // ham.innerHTML = '<span class="material-symbols-outlined" id="menu_icon">close</span>'
+    if(i % 2 == 0) {
+        ham.textContent="close";
+        i++;
+    } else {
+        ham.textContent="menu";
+        i++;
+    };
 })
 
 
 // 모바일 화면에서 네비게이션 메뉴 클릭 시 메뉴 숨김 //
 navMenu.addEventListener('click', ()=> {
     navMenu.classList.remove('open');
-
+    // ham.innerHTML = '<span class="material-symbols-outlined" id="menu_icon">menu</span>'
+    if(i % 2 == 0) {
+        ham.textContent="close";
+        i++;
+    } else {
+        ham.textContent="menu";
+        i++;
+    };
 })
+
